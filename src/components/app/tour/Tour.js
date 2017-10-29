c6c5d222efd0463264c6e7ad9fd61e8cf0cb45d7
George Paul <george.allen.paul.3@gmail.com>
2017-10-28 22:07:42 -0400

    Add more styling

--- src/components/app/tour/Tour.js
+++ src/components/app/tour/Tour.js
@@ -4,7 +4,7 @@ class Tour extends Component {
   render () {
     return (
       <div style={{flex: 1}}>
-        <h1 style={{color: 'white'}}>No upcoming live shows</h1>
+        <h1 style={{color: 'lightgrey'}}>No upcoming live shows</h1>
       </div>
     )
   }
