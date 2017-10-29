491c74d19a76fe3f1bde3f04830ddb4245f2efb9
George Paul <george.allen.paul.3@gmail.com>
2017-10-28 20:36:16 -0400

    Add google font and scrollTo logic

--- src/components/app/header/Header.js
+++ src/components/app/header/Header.js
@@ -2,11 +2,16 @@ import React from 'react'
 import logo from './MR_Logo.png'
 import './Header.css'
 
-const Header = () => (
+const Header = ({ scrollToRef }) => (
   <header className='App-header'>
     <img src={logo} className='App-logo' alt='logo' />
+    <ul className='header-items'>
+      <li onClick={scrollToRef('player')}>Music</li>
+      <li>Tour</li>
+      <li>Merch</li>
+      <li>Contact</li>
+    </ul>
   </header>
-
 )
 
 export default Header
